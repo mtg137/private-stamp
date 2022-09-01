3526cd09554312f3da271206d8bcca454ee84407
mtg137 <jony374@outlook.com>
2022-09-01 01:19:43 -0400

    move toasted plugin registration

--- app/src/main.js
+++ app/src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import Vuex from "vuex";
 import drizzleVuePlugin from "@drizzle/vue-plugin";
 import drizzleOptions from "./drizzleOptions";
-import vuetify from './plugins/vuetify';
+import VueIpfs from "./plugins/vue-ipfs";
+import Toasted from "vue-toasted";
 
 // Register Vuex
 Vue.use(Vuex);
@@ -11,8 +12,14 @@ Vue.use(Vuex);
 // Create and configure your Vuex Store
 const store = new Vuex.Store({ state: {} });
 
+// Load our IPFS plugin.
+Vue.use(VueIpfs);
+
 // Register the drizzleVuePlugin
 Vue.use(drizzleVuePlugin, { store, drizzleOptions });
+
+// Register Toasted
+Vue.use(Toasted);
 
 Vue.config.productionTip = false;
 
